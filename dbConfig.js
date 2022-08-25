521450c3da1648dad535012a15a9acd2b9a40312
Ahindra D <official.ahindra@gmail.com>
2022-08-25 17:54:42 +0530

    elephantSQL not responding

--- dbConfig.js
+++ dbConfig.js
@@ -1,14 +1,17 @@
 const pool = require("pg").Client;
 require("dotenv").config();
 
-// const dbPool = new pool({
-//     user: "postgres",
-//     host: "127.0.0.1",
-//     database: "shortit",
-//     password: "986619",
-//     port: 5432,
-// })
+/*
+const pool = require("pg").Pool;
+const dbPool = new pool({
+    user: "postgres",
+    host: "127.0.0.1",
+    database: "shortit",
+    password: "986619",
+    port: 5432,
+})
+*/
 
-const dbPool=new pool(process.env.URL);
+const dbPool = new pool(process.env.URL);
 
 module.exports = dbPool;
