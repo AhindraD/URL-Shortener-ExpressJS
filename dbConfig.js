61284df8db25661f78c0e75eb222e0006ace36e1
Ahindra D <official.ahindra@gmail.com>
2022-08-25 17:22:59 +0530

    done

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,14 @@
 const pool = require("pg").Pool;
+require("dotenv").config();
 
-const dbPool = new pool({
-    user: "postgres",
-    host: "127.0.0.1",
-    database: "shortit",
-    password: "986619",
-    port: 5432,
-})
+// const dbPool = new pool({
+//     user: "postgres",
+//     host: "127.0.0.1",
+//     database: "shortit",
+//     password: "986619",
+//     port: 5432,
+// })
+
+const dbPool=new pool(process.env.URL);
 
 module.exports = dbPool;
